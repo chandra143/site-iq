c85a79de94a19062cfdc10e46e357dd96aa3e12f
Chandra Sekhar Yallanti <s8121713016@gmail.com>
2019-09-28 18:40:36 +0530

    Intial Commit

--- dev-server.js
+++ dev-server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const app = express(); 
 if (process.env.NODE_ENV !== 'production') {
     const webpackMiddleware = require('webpack-dev-middleware');
     const webpack = require('webpack');
